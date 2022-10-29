56eff820a10cc1fea1ed7921f89c310fccfad750
Nandkishor <nandkishorcdacacts@gmail.com>
2022-10-29 11:48:27 +0530

    cta part

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
- 
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNavigation = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector(".primary-header");
+
+navToggle.addEventListener("click", () => {
+  primaryNavigation.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNavigation.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
